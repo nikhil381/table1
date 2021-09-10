2d5b39068c07ae1584366d4e296bae12e480bd03
nikhil381 <nikhilappletech123@gmail.com>
2021-09-10 13:19:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-material-table-data-source-bmhz2e
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-material-table-data-source-bmhz2e)
---
+++ app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MatTableModule ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
